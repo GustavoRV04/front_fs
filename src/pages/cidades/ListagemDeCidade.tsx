339b1778d134350150661e0a6350d39475a83f4e
Gustavo <gustavoroviana@gmail.com>
2025-11-21 13:30:15 -0300

    pagina cidades

---
+++ src/pages/cidades/ListagemDeCidade.tsx
@@ -0,0 +1,31 @@
+import { useSearchParams } from "react-router-dom";
+import { useMemo } from "react";
+
+import { FerramentasDaListagem } from "../../shared/components";
+import { LayoutBaseDePagina } from "../../shared/layouts";
+
+
+export const ListagemDeCidade: React.FC = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const busca = useMemo(() => {
+        return searchParams.get('busca') || '';
+    }, [searchParams]);
+
+    return (
+        <LayoutBaseDePagina 
+        titulo='Listagem de cidades'
+        barraDeFerramentas={<FerramentasDaListagem
+            textoBotaoNovo="Nova"
+            mostrarInputBusca
+            textoDaBusca={busca}
+            aoMudarTextoDaBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
+            />
+        }
+
+        >
+            
+        
+        </LayoutBaseDePagina>
+    );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useDrawerContext } from "../shared/contexts";
 import { useEffect } from "react";
-import { Dashboard } from "../pages";
+import { Dashboard, ListagemDeCidade } from "../pages";
 
 export const AppRoutes = () => {
     const { setDrawerOptions } = useDrawerContext();
@@ -13,12 +13,20 @@ export const AppRoutes = () => {
                 path: '/pagina-inicial',
                 label: 'Página Inicial',
             },
+            {
+                icon: 'location_city',
+                path: '/cidades',
+                label: 'Cidades',
+            },
         ]);
     }, []);
 
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<Dashboard/>} />  
+            <Route path="/pagina-inicial" element={<Dashboard/>} /> 
+
+            <Route path="/cidades" element={<ListagemDeCidade/>} />
+            <Route path="/cidades/detalhe/:id" element={<Dashboard/>} />    
 
             <Route path="*" element={<Navigate to= "/pagina-inicial"/>} />  
         </Routes>
